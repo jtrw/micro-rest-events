354e04ab7e9111edf2944292ed9d5e6bbb067b5e
Nil Borodulia <nil.b@oxtech.org>
2023-08-04 15:41:09 +0300

    init project

---
+++ app/backend/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+   "context"
+   "log"
+   "time"
+   "github.com/jessevdk/go-flags"
+   "fmt"
+   server "go-rest-events/v1/app/backend/server"
+   "go-rest-events/v1/app/backend/repository"
+   "github.com/joho/godotenv"
+   _ "github.com/lib/pq"
+)
+
+type Server struct {
+	PinSize        int
+	MaxPinAttempts int
+	WebRoot        string
+	Version        string
+	Port           string
+}
+
+type Options struct {
+    Port string `short:"p" long:"port" env:"SERVER_PORT" default:"8080" description:"Port web server"`
+    PinSize int `long:"pinszie" env:"PIN_SIZE" default:"5" description:"pin size"`
+    MaxExpire time.Duration `long:"expire" env:"MAX_EXPIRE" default:"24h" description:"max lifetime"`
+    MaxPinAttempts int `long:"pinattempts" env:"PIN_ATTEMPTS" default:"3" description:"max attempts to enter pin"`
+    WebRoot string `long:"web" env:"WEB" default:"/" description:"web ui location"`
+}
+
+var revision string
+
+func main() {
+    if err := godotenv.Load(); err != nil {
+        panic("No .env file found")
+    }
+
+    var opts Options
+    parser := flags.NewParser(&opts, flags.Default)
+    _, err := parser.Parse()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("Micro rest events %s\n", revision)
+    repo := repository.ConnectDB()
+
+    srv := server.Server{
+        Port:           opts.Port,
+        PinSize:        opts.PinSize,
+        MaxExpire:      opts.MaxExpire,
+        MaxPinAttempts: opts.MaxPinAttempts,
+        WebRoot:        opts.WebRoot,
+        Version:        revision,
+        Repository:     repo,
+    }
+    if err := srv.Run(context.Background()); err != nil {
+        log.Printf("[ERROR] failed, %+v", err)
+    }
+}
