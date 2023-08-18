3b35e45d7a2c74f9d1ab60bc561b74530c334df3
Nil Borodulia <nil.b@oxtech.org>
2023-08-18 12:42:24 +0300

    Add test case for unauthorized access to user's events

--- app/backend/main_test.go
+++ app/backend/main_test.go
@@ -20,6 +20,7 @@ import (
 type JSON map[string]interface{}
 
 var jwtToken string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxMjM0NX0.0JbSlLakm5e2a4zkpTDsVGRCV_YMvyK7lWga4C6t8WQ"
+var jwtTokenWithoutUserId string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub3RfdXNlciI6MTIzNDV9.R8Ze0IKMUdY5N7oq5kYpkPTL_la5ZfLz-3wZolYbCqo"
 
 func Test_main(t *testing.T) {
 	port := 40000 + int(rand.Int31n(10000))
@@ -167,6 +168,22 @@ func Test_Fail_Auth(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, "Invalid token\n", string(body))
 	}
+
+	{
+        url := fmt.Sprintf("http://localhost:%d/api/v1/events/users/1", port)
+        req, _ := getRequest(url)
+
+        req, err := http.NewRequest("GET", url, nil)
+        req.Header.Add("Api-Token", jwtTokenWithoutUserId)
+
+        resp, err := client.Do(req)
+        require.NoError(t, err)
+        defer resp.Body.Close()
+        assert.Equal(t, 401, resp.StatusCode)
+        body, err := io.ReadAll(resp.Body)
+        assert.NoError(t, err)
+        assert.Equal(t, "user_id not found\n", string(body))
+    }
 }
 
 func getRequest(url string) (*http.Request, error) {
