f8b2f1b52ad8a5ace815e19549b0dd51a2e95f7f
Nil Borodulia <nil.b@oxtech.org>
2023-08-24 11:55:30 +0300

    Add event handler test and connect to PostgreSQL database

---
+++ app/backend/handler/event_handler_test.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+    "net/http"
+    "testing"
+    "net/http/httptest"
+    "micro-rest-events/v1/app/backend/repository"
+    event_handler "micro-rest-events/v1/app/backend/handler"
+    "github.com/stretchr/testify/assert"
+    //"github.com/stretchr/testify/require"
+)
+func TestOnGetEventsByUserIdNotFound(t *testing.T) {
+
+    t.Setenv("POSTGRES_DSN", "host=localhost port=5432 user=event password=9ju17UI6^Hvk dbname=micro_events sslmode=disable")
+    repo := repository.ConnectDB()
+    h := event_handler.NewHandler(repo.Connection)
+    // Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+    // pass 'nil' as the third parameter.
+    req, err := http.NewRequest("GET", "/events/users/9999999", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+    // We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(h.OnGetEventsByUserId)
+    // Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+    // directly and pass in our Request and ResponseRecorder.
+    handler.ServeHTTP(rr, req)
+
+     assert.NoError(t, err)
+     assert.Equal(t, 404, rr.Code)
+
+     //assert.Equal(t, "{\"status\": \"not_found\", \"message\": \"Not Found\"}", rr.Body.String())
+}
--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/didip/tollbooth_chi v0.0.0-20220719025231-d662a7f6928f
 	github.com/go-chi/chi/v5 v5.0.10
 	github.com/go-chi/render v1.0.3
-	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.3.0
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/joho/godotenv v1.5.1
@@ -21,6 +20,7 @@ require (
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/go-pkgz/expirable-cache v0.1.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
