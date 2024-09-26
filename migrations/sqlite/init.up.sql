ffc42636785495f240274f4af9ca59eb402262a8
Nil Borodulia <nil.borodulia@gmail.com>
2024-09-26 10:56:06 +0200

    Update SQLite migration scripts for creating events table

--- migrations/sqlite/init.up.sql
+++ migrations/sqlite/init.up.sql
@@ -1,3 +1,18 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-COMMIT;
+CREATE TABLE events (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    uuid TEXT NOT NULL,
+    user_id TEXT NULL,
+    type TEXT NULL,
+    status TEXT NULL,
+    caption TEXT NULL,
+    message TEXT NULL,
+    is_seen BOOLEAN DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX events_user_id_idx ON events (user_id);
+
+CREATE INDEX events_status_index ON events (status);
+
+CREATE INDEX events_user_id_status_index ON events (user_id, status);
