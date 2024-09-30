5f8d99bfaf11b3cff5d198173cedd0a8ac1f3339
Nil Borodulia <nil.borodulia@gmail.com>
2024-09-30 11:43:14 +0200

    Update workflow file and remove unused PostgreSQL service

--- app/backend/main_test.go
+++ app/backend/main_test.go
@@ -25,7 +25,6 @@ var jwtTokenWithoutUserId string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub3R
 
 func Test_main(t *testing.T) {
 	port := 40000 + int(rand.Int31n(10000))
-	//os.Args = []string{"app", "--secret=123", "--listen=" + "localhost:"+strconv.Itoa(port), "--dsn=host=localhost port=5532 user=event password=9ju17UI6^Hvk dbname=micro_events sslmode=disable"}
 	os.Args = []string{"app", "--secret=123", "--listen=" + "localhost:" + strconv.Itoa(port), "--conn=file:///tmp/test_events.db"}
 
 	done := make(chan struct{})
